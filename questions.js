1d8110155c8d70661c224f7c0dc60871da57c930
English-For-Life <83182532+English-For-Life@users.noreply.github.com>
2021-07-10 12:33:12 +0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,82 @@
+//Array of objects
+const quiz = [
+
+    {
+        q:"We _____ to the city museum three days ago.",
+        options:["went", "can go", "go", "are going"],
+        answer: 0
+    },
+    {
+        q:"Victor _____ some bad news from Ted yesterday and _____ sad.",
+        options:["can hear / become", "heard / became", "hears / becomes", "is hearing / is becoming"],
+        answer: 1
+    },
+    {
+        q:"_____ she _____ your books back last week?",
+        options:["Is / bringing", "Does / bring", "Did / bring", "Can / bring"],
+        answer: 2
+    },
+    {
+        q:"I _____ my house last week, but it _____ dirty again now.",
+        options:["cleaned / was", "am cleaning / is", "clean / was", "cleaned / is"],
+        answer: 3
+    },
+    {
+        q:"John _____ an operation on his stomach last year and now he _____ no problem with it.",
+        options:["had / has", "can have / had", "is having / can have", "has / had"],
+        answer: 0
+    },
+    {
+        q:"I _____ the window and _____ a deep breath before the exam yesterday.",
+        options:["am opening / am taking", "opened / took", "open / take", "can open / can take"],
+        answer: 1
+    },
+    {
+        q:"Nina _____ in the park alone yesterday morning and she _____ a book.",
+        options:["can sit / reads", "sits / reads", "sat / read", "is sitting / is reading"],
+        answer: 2
+    },
+    {
+        q:"When _____ your clients sign the document? - Four days ago.",
+        options:["are", "were", "was", "did"],
+        answer: 3
+    },
+    {
+        q:"Thomas _____ up in the morning and _____ his best clothes because that day was important for him.",
+        options:["woke / wore", "is waking / is wearing", "wakes / wears", "can wake / wore"],
+        answer: 0
+    },
+    {
+        q:"Mandy _____ her arm two weeks ago and she _____ now.",
+        options:["breaks / isn't writing", "broke / can't write", "is breaking / doesn't write", "broke / didn't write"],
+        answer: 1
+    },
+    {
+        q:"The police officer _____ the driver right now because he _____ a traffic rule a few minutes ago.",
+        options:["is talking / broke", "can talk / is breaking", "talks / breaks", "talked / broke"],
+        answer: 0
+    },
+    {
+        q:"He _____ his homework but _____ it at home because he left home in a hurry yesterday morning.",
+        options:["is doing / forgets", "did / forgot", "can do / is forgetting", "do / forgot"],
+        answer: 1
+    },
+    {
+        q:"I always _____ up late in the past because I was unemployed, but I'm working now and _____ up early to go to the office on time.",
+        options:["get / got", "am getting / get", "got / get", "can get / are getting"],
+        answer: 2
+    },
+    {
+        q:"We _____ very hard last night to finish the project on time, but it is weekend now and we _____ a break.",
+        options:["worked / had", "are working / have", "work / had", "worked / are having"],
+        answer: 3
+    },
+    {
+        q:"What _____ you tell Tom last night? - I _____ him the truth.",
+        options:["did / told", "were / tell", "were / told", "did / tell"],
+        answer: 0
+    }
+
+
+
+]
